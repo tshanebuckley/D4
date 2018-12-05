586584f49b321f14777f7682170c4fc45fc3eaa2
Shane Buckley <tshanebuckley@gmail.com>
2018-12-05 13:53:21 -0500

    Update word_finder.rb
    attemp at parallelism

--- word_finder.rb
+++ word_finder.rb
@@ -1,6 +1,8 @@
 require_relative 'graph.rb'
 require_relative 'node.rb'
-require_relative 'runner'
+require_relative 'runner.rb'
+# require 'flamegraph'
+require 'parallel'
 # initialize DAG
 text = ARGV[0].to_s
 g = Graph.new(text)
@@ -8,8 +10,17 @@ g = Graph.new(text)
 paths = g.find_paths
 # paths.each { |x| puts(x) }
 # find all permutations
-perms = []
-paths.each { |x| x.permutations.each { |i| perms.push(i) } }
+# perms = []
+# puts(paths.size)
+paths = paths.in_groups(4)
+# puts(paths.size)
+perms = Parallel.map(paths, in_processes: 4) do |x|
+  x.map(&:permutations)
+end
+perms = perms.flatten
+# perms.each { |x| puts(x) }
+# paths.each { |x| print(x); puts }
+# paths.each { |x| x.permutations.each { |i| perms.push(i) } }
 # perms.each {|x| puts(x)}
 words = get_words 'wordlist.txt'
 # puts(words.to_s)
